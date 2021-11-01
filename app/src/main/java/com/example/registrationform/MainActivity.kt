33b2c59a36098dd6ca3ca460bb324afff3c832bc
khulud <Khulud.Saleh.ruh.k@tuwaiq.edu.sa>
2021-11-01 13:17:51 +0300

    the code is great now :)

--- app/src/main/java/com/example/registrationform/MainActivity.kt
+++ app/src/main/java/com/example/registrationform/MainActivity.kt
@@ -2,47 +2,48 @@ package com.example.registrationform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.textservice.TextInfo
-
 import com.example.registrationform.databinding.ActivityMainBinding
-import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-
-        binding.regButton.setOnClickListener{ regesration() }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.regButton.setOnClickListener { regesration() }
     }
-     fun regesration() {
-         //=================name part=====================
-         val stringInTextFiled = binding.PersonName.text
-         //=================email part=====================
-         val stringInEmail = binding.EmailAddress.text
-        //=================birthdate part=====================
-         val stringInDataInput = binding.birthDay.toString()
-         //=================gender selection part=====================
-         val selectedGender = binding.radioGroup.checkedRadioButtonId
-         val gender = when (selectedGender) {
-             R.id.radioFemale -> "Female"
-             else -> "Male"         }
-         //=================password part=====================
-         val stringInPasswor = binding.Password.text
-         val stringInRePassword = binding.rePassword.text
-         val missMatch = if (stringInPasswor != stringInRePassword) {
-             println("the password not matching!!plase re_enter a gain .")
-         } else {  binding.output.setText(    "Your Info is /n " +
-                          "================= /n " +
-                         "Name: $stringInTextFiled /n " +
-                         "Email $stringInEmail /n" +
-                         "password: $stringInPasswor /n " +
-                         "re_password $stringInRePassword /n" +
-                         "Birth Date: $stringInDataInput /n " +
-                         "Gender $gender /n" )
-         }
 
-     }
+    fun regesration() {
+        val stringInTextFiled = binding.PersonName.text.toString()
+        val stringInEmail = binding.EmailAddress.text.toString()
+        val stringInPasswor = binding.Password.text.toString()
+        val stringInRePassword = binding.rePassword.text.toString()
+        val stringInDataInput = binding.birthDay.text
+        val selectedGender = binding.radioGroup.checkedRadioButtonId
+        val gender = when (selectedGender) {
+            R.id.radioFemale -> "Female"
+            else -> "Male"
+        }
 
-     }
+        if (stringInDataInput.isNotEmpty() && stringInEmail.isNotEmpty() && stringInPasswor.isNotEmpty() && stringInRePassword.isNotEmpty() && stringInDataInput.isNotEmpty()) {
+            if (stringInPasswor.toString() == stringInRePassword.toString()) {
+                binding.output.setText(
+                    "Your Info is \n " +
+                            "================= \n " +
+                            "Name: $stringInTextFiled  \n " +
+                            "Email $stringInEmail \n" +
+                            "password: $stringInPasswor \n " +
+                            "re_password $stringInRePassword \n" +
+                            "Birth Date: $stringInDataInput \n " +
+                            "Gender $gender "
+                )
+            } else {
+                binding.output.setText("the password not matching!!please re_enter a gain ")
+            }
+
+        } else {
+            binding.output.setText("fill the all the fields right")
+        }
+
+    }
+}
