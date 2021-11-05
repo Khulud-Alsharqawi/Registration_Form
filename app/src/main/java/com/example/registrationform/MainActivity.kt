d068d2db9fd02657fd275adb81d41d2cd41cec09
khulud <Khulud.Saleh.ruh.k@tuwaiq.edu.sa>
2021-11-05 16:13:40 +0300

    Finally -> check pw patterns

--- app/src/main/java/com/example/registrationform/MainActivity.kt
+++ app/src/main/java/com/example/registrationform/MainActivity.kt
@@ -4,6 +4,10 @@ package com.example.registrationform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PatternMatcher
+import android.provider.ContactsContract
+import android.util.Patterns
+import android.widget.Toast
 import com.example.registrationform.databinding.ActivityMainBinding
 
 
@@ -16,36 +20,35 @@ class MainActivity : AppCompatActivity() {
         binding.regButton.setOnClickListener { regeneration() }
     }
 
-
     fun regeneration() {
-        val stringInTextFiled = binding.PersonName.editText?.text.toString()
-        val stringInEmail = binding.EmailAddress.editText?.text.toString()
-        val stringInPassword = binding.Password.editText?.text.toString()
-        val stringInRePassword = binding.rePassword.editText?.text.toString()
-        val stringInDataInput = binding.birthDay.editText?.text.toString()
+        val stringInTextFiled = binding.userName.text.toString()
+        val stringInEmail = binding.userEmail.text.toString()
+        val stringInPassword = binding.userPw.text.toString()
+        val stringInRePassword = binding.userRePw.text.toString()
+        val stringInDataInput = binding.userBD.text.toString()
         val gender = when (binding.radioGroup.checkedRadioButtonId) {
             R.id.radioFemale -> "Female"
             else -> "Male"
         }
 
 
-    if (stringInDataInput!!.isNotEmpty() && stringInEmail.isNotEmpty() && stringInPassword.isNotEmpty()
+    if (stringInDataInput.isNotEmpty() && stringInEmail.isNotEmpty() && stringInPassword.isNotEmpty()
         && stringInRePassword.isNotEmpty() && stringInDataInput.isNotEmpty()) {
-                if (stringInPassword == stringInRePassword) {
-                    binding.output.text = "Your Info is \n " +
-                            "================= \n " +
-                            "Name: $stringInTextFiled  \n " +
-                            "Email $stringInEmail \n" +
-                            "password: $stringInPassword \n " +
-                            "re_password $stringInRePassword \n" +
-                            "Birth Date: $stringInDataInput \n " +
-                            "Gender $gender "
+        if (!Patterns.EMAIL_ADDRESS.matcher(stringInEmail).matches())            {
+
+        Toast.makeText(this, "Invalid Email Address, Please Enter right email form ", Toast.LENGTH_SHORT).show()
+
+        }
+            else    if (stringInPassword == stringInRePassword) {
+                    binding.output.text = "Hello: $stringInTextFiled, your Email $stringInEmail,and your password is: $stringInPassword  "
                 } else {
-                    binding.output.text = "the password not matching!!please re_enter a gain "
+                    Toast.makeText(this,"the password not matching!!please re_enter a gain",Toast.LENGTH_SHORT).show()
+                  //  binding.output.text = "the password not matching!!please re_enter a gain "
                 }
 
             } else {
-                binding.output.text = "fill the all the fields right"
+                Toast.makeText(this,"fill the all the fields right", Toast.LENGTH_SHORT).show()
+                // binding.output.text = "fill the all the fields right"
             }
         }
     }
